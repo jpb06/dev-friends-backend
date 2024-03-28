2373d4e7bf11e05da4b7a696be9227d12de44429
jpb06 <jpb.06@outlook.com>
2024-03-28 17:00:26 +0100

    chore: extending pagination args

--- src/modules/squads/dto/squads-developers.result.dto.ts
+++ src/modules/squads/dto/squads-developers.result.dto.ts
@@ -3,8 +3,10 @@ import { Exclude, Expose, Type } from 'class-transformer';
 
 import { DevDto } from '../../devs/dto/dev.dto';
 
+import { PaginatedResponseDto } from '@decorators/dto/paginated-response.dto';
+
 @Exclude()
-export class SquadsDevelopersResultDto {
+export class SquadsDevelopersResultDto extends PaginatedResponseDto {
   @Expose()
   @ApiProperty({ isArray: true, type: DevDto })
   @Type(() => DevDto)
--- src/modules/squads/squads.controller.ts
+++ src/modules/squads/squads.controller.ts
@@ -9,12 +9,15 @@ import {
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { delay, map, Observable } from 'rxjs';
 
+import { ParsePositiveIntegerPipe } from '../../pipes/parse-positive-integer/parse-positive-integer.pipe';
+
 import { AllSquadsResultDto } from './dto/all-squads.result.dto';
 import { SquadsDevelopersResultDto } from './dto/squads-developers.result.dto';
 
 import { ApiRoute } from '@decorators/api-route';
 import { DevsStoreService } from '@modules/dal/stores/devs-store.service';
 import { SquadsStoreService } from '@modules/dal/stores/squads-store.service';
+import { createPaginatedResponse } from 'logic/create-paginated-response.logic';
 
 @Controller('squads')
 @ApiTags('squads')
@@ -65,13 +68,28 @@ export class SquadsController {
     required: false,
     type: Number,
   })
+  @ApiQuery({
+    name: 'page',
+    description: 'Page to fetch, starting at 1',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'byPage',
+    description: 'Numbers of developers to fetch per page',
+    required: false,
+    type: Number,
+  })
   getSquadsDevelopers(
     @Query('delayMs', new DefaultValuePipe(0), ParseIntPipe) delayMs: number,
+    @Query('page', new DefaultValuePipe(1), ParsePositiveIntegerPipe)
+    page: number,
+    @Query('byPage', new DefaultValuePipe(20), ParsePositiveIntegerPipe)
+    byPage: number,
     @Param('id', new ParseIntPipe()) idSquad: number,
   ): Observable<SquadsDevelopersResultDto> {
-    return this.devsStore.getBy(idSquad).pipe(
-      delay(delayMs),
-      map((devs) => ({ result: devs })),
-    );
+    return this.devsStore
+      .getBy(idSquad)
+      .pipe(delay(delayMs), map(createPaginatedResponse(page, byPage)));
   }
 }
